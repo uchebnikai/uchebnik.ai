a533945186e9b9c6e7c52e5596b39aa15e7200fd
uchebnikai <uchebnikai@gmail.com>
2025-12-18 01:05:36 +0200

    Delete styles directory

--- styles/utils.ts
+++
@@ -1,2 +0,0 @@
-export const getBackgroundImageStyle = (url: string | null) => url ? { backgroundImage: `url(${url})` } : {};
-export const getDynamicHeightStyle = (height: number) => ({ height: `${height}px` });
