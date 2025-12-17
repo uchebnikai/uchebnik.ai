2af1935908bc170a589877ac77cd7f137ebdd4fe
uchebnikai <uchebnikai@gmail.com>
2025-12-18 01:04:07 +0200

    Delete components directory

--- components/ui/DynamicIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import * as LucideIcons from 'lucide-react';
-
-export const DynamicIcon = ({ name, className }: { name: string, className?: string }) => {
-  const Icon = (LucideIcons as any)[name];
-  return Icon ? <Icon className={className} /> : <LucideIcons.HelpCircle className={className} />;
-};
